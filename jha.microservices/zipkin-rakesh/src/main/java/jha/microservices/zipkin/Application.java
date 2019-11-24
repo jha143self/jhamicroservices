2fb18f5152c8b32e23d4475afb4db4ca7c8b50c4
jha143self <jha143@gmail.com>
2019-11-23 20:59:12 -0800

    First

---
+++ jha.microservices/zipkin-rakesh/src/main/java/jha/microservices/zipkin/Application.java
@@ -0,0 +1,17 @@
+package jha.microservices.zipkin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
